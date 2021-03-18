8b17aebf48f6f87d04b93eabff536ff61ea8e184
sr5864 <sr5864@nyu.edu>
2021-03-18 00:17:24 -0400

    sqli

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+# class MyTestCase(TestCase):
+#     fixtures = ['testdata.json']
+#     def setUp(self):
+#         self.client = Client()
+#     def test_details(self):
+#         response = self.client.get('/buy.html')
+#         print(response.content)
--- LegacySite/views.py
+++ LegacySite/views.py
@@ -191,7 +191,7 @@ def use_card_view(request):
         print(card_data.strip())
         signature = json.loads(card_data)['records'][0]['signature']
         # signatures should be pretty unique, right?
-        card_query = Card.objects.raw('select id from LegacySite_card where data = \'%s\'' % signature)
+        card_query = Card.objects.raw('select id from LegacySite_card where data = \'%s\'')
         user_cards = Card.objects.raw('select id, count(*) as count from LegacySite_card where LegacySite_card.user_id = %s' % str(request.user.id))
         card_query_string = ""
         for thing in card_query:
